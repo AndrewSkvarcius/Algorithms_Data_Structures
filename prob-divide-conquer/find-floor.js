ec0e1f67ec0c5582c998a5cfe31142e2e1a11b1f
[Andrew Skvarcius] <andrewskvarcius@gmail.com>
2023-11-25 18:27:21 -0500

    finished exercise

--- prob-divide-conquer/find-floor.js
+++ prob-divide-conquer/find-floor.js
@@ -1,5 +1,21 @@
-function findFloor() {
-  
-}
+function findFloor(arr, x) {
+    let start = 0;
+    let end = arr.length - 1;
+    let floor = -1;
 
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if (arr[mid] === x) {
+            return arr[mid]; // x is found in the array
+        } else if (arr[mid] < x) {
+            floor = arr[mid]; // arr[mid] could be a potential floor
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    
+    return floor; // If x is not found, return the last floor value found
+}
 module.exports = findFloor
